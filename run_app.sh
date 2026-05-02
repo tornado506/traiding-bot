5425b3c5edf7cb49e51988952278c13fb6e88333
tornado506 <tornado506@naver.com>
2026-05-02 10:20:43 +0900

    sh 파일들 깃허브 서버에서 제거 완료

--- run_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Documents/bot_project
-source venv_mac/bin/activate
-streamlit run app.py --server.port 8501 --server.address localhost
--- run_app2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Documents/bot_project
-source venv_mac/bin/activate
-streamlit run app2.py --server.port 8502 --server.address localhost
